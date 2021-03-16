a16b8730ed4b09226b70dac0964fbea9161ae38c
João Paludo <jas.paludo@gmail.com>
2021-03-16 09:36:42 -0300

    Finaliza adicionando mais produtos

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -3,7 +3,7 @@ import useFetch from "../hooks/useFetch"
 import ProductCard from "../components/ProductCard"
 
 function Catalog() {
-  const { data: products, isLoading, error } = useFetch("/products?limit=3")
+  const { data: products, isLoading, error } = useFetch("/products?limit=15")
 
   if (isLoading) return <h2>Loading...</h2>
   if (error) return <h2>Error fetching data!</h2>
